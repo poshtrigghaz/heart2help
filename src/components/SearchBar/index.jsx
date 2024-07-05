1d279bd4e2a0b5727f2f81cf68a2d847ccceeead
Nana Kwame <105567071+chiro999@users.noreply.github.com>
2024-07-05 03:54:28 +0000

    Correcting job values set in Firebase and removing redundant filters

--- src/components/SearchBar/index.jsx
+++ src/components/SearchBar/index.jsx
@@ -18,15 +18,9 @@ function SearchBar(props) {
     const search = async() => {
         await props.fetchJobsCustom(jobCriteria);
     }
-    
 
   return (
     <div className='flex gap-4 my-10 justify-center px-10'>
-        <select onChange={handleChange} name="title" value={jobCriteria.title} className='w-64 py-3 pl-4 bg-zinc-200 font-semibold rounded-md'>
-            <option value="" disabled hidden>Job Role</option>
-            <option value="Medical Internship">Medical Internship</option>
-            <option value="Education Support">Education Support</option>
-        </select>
         <select onChange={handleChange} name="type" value={jobCriteria.type} className='w-64 py-3 pl-4 bg-zinc-200 font-semibold rounded-md'>
             <option value="" disabled hidden>Job Type</option>
             <option value="Full Time">Full Time</option>
@@ -39,14 +33,9 @@ function SearchBar(props) {
             <option value="On-site">On-site</option>
             <option value="Hybrid">Hybrid</option>
         </select>
-        <select onChange={handleChange} name="experience" value={jobCriteria.experience} className='w-64 py-3 pl-4 bg-zinc-200 font-semibold rounded-md'>
-            <option value="" disabled hidden>Experience</option>
-            <option value="Medical Clearance">Medical Clearance</option>
-            <option value="SSSCE/WASSCE Clearance">SSSCE/WASSCE Clearance</option>
-        </select>
         <button onClick={search} className='w-64 bg-blue-500 text-white font-bold py-3 rounded-md'>Search</button>
     </div>
   )
 }
-
-export default SearchBar
+ 
+export default SearchBar;
