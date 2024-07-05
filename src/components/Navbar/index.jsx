2355ac929f5798b5baac050657d27e7a2b528066
dtemilade <dtemilade@gmail.com>
2024-07-05 01:19:25 +0100

    Adding Open Form Button to Navbar component

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -14,12 +14,21 @@ function Navbar() {
   return (
     <div className='h-20 flex items-center justify-between w-full text-white bg-blue-500'>
       <div className='text-3xl pl-20 font-bold'>heart2help</div>
-      <button 
-        onClick={handleLogout} 
-        className='bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded mr-20'
-      >
-        Logout
-      </button>
+      <div className='flex items-center'>
+        <a 
+          href="../../../JobForm.html" 
+          className='bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded mr-4'
+          target="_blank" // Remove this if you want to open in the same tab
+        >
+          Open Form
+        </a>
+        <button 
+          onClick={handleLogout} 
+          className='bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded mr-20'
+        >
+          Logout
+        </button>
+      </div>
     </div>
   );
 }
