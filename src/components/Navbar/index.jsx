8916fc05438ec8df642f2c50ce2b990e675aea73
Nana Kwame <105567071+chiro999@users.noreply.github.com>
2024-07-08 01:24:16 +0000

    making navbar color transparent

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -36,7 +36,7 @@ function Navbar() {
   };
 
   return (
-    <div className='h-20 flex items-center justify-between w-full text-white bg-blue-500'>
+    <div className='h-20 flex items-center justify-between w-full text-white bg-blue-500 bg-opacity-0'>
       <Link to="/" className='text-3xl pl-20 font-bold'>heart2help</Link>
       <div className='flex items-center'>
         <Link 
